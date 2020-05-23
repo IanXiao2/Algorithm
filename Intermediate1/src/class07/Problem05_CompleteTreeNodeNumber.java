1400c44af394fec0c5cab27b3b864458b3c45278
IanXiao2 <2640988921@qq.com>
2020-05-23 12:50:52 +0800

    Test3

--- Intermediate1/src/class07/Problem05_CompleteTreeNodeNumber.java
+++ Intermediate1/src/class07/Problem05_CompleteTreeNodeNumber.java
@@ -3,6 +3,6 @@ package class07;
 public class Problem05_CompleteTreeNodeNumber {
 
     public static void main(String[] args) {
-        System.out.println("完全二树");
+        System.out.println("完全");
     }
 }
