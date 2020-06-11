7365752e4498727a76d1ec82b777a7e3899eae21
IanXiao2 <2640988921@qq.com>
2020-06-11 15:53:08 +0800

    栈——只利用一个辅助栈对无序栈进行排序

---
+++ Intermediate1/src/class02/Problem05_StackSortStack.java
@@ -0,0 +1,44 @@
+package class02;
+
+import java.util.Stack;
+
+public class Problem05_StackSortStack {
+
+
+    public static void sortStackByStack(Stack<Integer> stack) {
+        Stack<Integer> help = new Stack<>();
+        int cur = 0;
+        while (!stack.isEmpty()) {
+            cur = stack.pop();
+            while (!help.isEmpty() && help.peek() < cur) {
+                stack.push(help.pop());
+            }
+            help.push(cur);
+        }
+
+        while (!help.isEmpty()) {
+            stack.push(help.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(1);
+        stack.push(6);
+        stack.push(2);
+        stack.push(5);
+        stack.push(4);
+        sortStackByStack(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
+
+}
