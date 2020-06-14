45321da2c59ec693cb9c17da4ad34486477e7c87
IanXiao2 <2640988921@qq.com>
2020-06-14 18:54:59 +0800

    数组——数组中未出现的数

--- Intermediate1/src/class07/Problem02_PrintNoInArray.java
+++ Intermediate1/src/class07/Problem02_PrintNoInArray.java
@@ -3,6 +3,9 @@ package class07;
 public class Problem02_PrintNoInArray {
 
 
+    //给定一个整数数组A，长度为n，有 1 <= A[i] <= n，且对于[1,n]的整数，其中部分整数会重复出现而部分不会出现。
+    // 实现算法找到[1,n]中所有未出现在A中的整数。
+
     public static void printNumberNoInArray(int[] arr) {
 
         if (arr == null || arr.length == 0) {
@@ -10,6 +13,7 @@ public class Problem02_PrintNoInArray {
         }
         for (int i = 0; i < arr.length; i++) {
             modify(arr, arr[i]);
+
         }
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != i + 1) {
@@ -32,6 +36,7 @@ public class Problem02_PrintNoInArray {
     public static void main(String[] args) {
 
         int[] test = { 3, 2, 3, 5, 6, 1, 6 };
+        //int[] test = {2, 3, 1, 0, 2, 5, 3};
         printNumberNoInArray(test);
 
     }
