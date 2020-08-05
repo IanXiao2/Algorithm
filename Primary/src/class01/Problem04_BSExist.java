a264174d6a83d3349b01634de4493b55aab41452
IanXiao2 <2640988921@qq.com>
2020-08-05 20:11:20 +0800

    二分查找——判断某个数是否存在于有序数组

--- Primary/src/class01/Problem04_BSExist.java
+++ Primary/src/class01/Problem04_BSExist.java
@@ -4,25 +4,29 @@ public class Problem04_BSExist {
 
 
     public static boolean exist(int[] arr, int num) {
+
         if (arr == null || arr.length < 1) {
             return false;
         }
+
         int l = 0;
         int r = arr.length - 1;
-        int mid = 0;
+        int m = 0;
         while (l < r) {
-            mid = l + ((r - l) >> 1);
-            if (arr[mid] == num) {
+            m = l + ((r - l)>>1);
+            if (arr[m] == num) {
                 return true;
-            } else if (arr[mid] > num) {
-                r = mid - 1;
+            } else if (arr[m] > num) {
+                r = m - 1;
             } else {
-                l = mid + 1;
+                l = m + 1;
             }
         }
-
         return arr[l] == num;
+
     }
+
+
 
     public static boolean exist2(int[] arr, int num) {
         if (arr == null || arr.length  < 1) {
