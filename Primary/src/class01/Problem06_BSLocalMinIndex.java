9306d6a2f1314d44fa73a9208d941131abc41c7c
IanXiao2 <2640988921@qq.com>
2020-08-05 20:13:20 +0800

    二分查找——局部最小值

--- Primary/src/class01/Problem06_BSLocalMinIndex.java
+++ Primary/src/class01/Problem06_BSLocalMinIndex.java
@@ -2,30 +2,29 @@ package class01;
 
 public class Problem06_BSLocalMinIndex {
 
-
     public static int localMinIndex(int[] arr) {
         if (arr == null || arr.length < 1) {
             return -1;
         }
+
+        int N = arr.length;
         if (arr.length == 1 || arr[0] < arr[1]) {
-            return 0;
-        }
-        int N = arr.length - 1;
-        if (arr[N - 1] < arr[N - 2]) {
-            return N - 1;
+            return arr[0];
         }
 
+        if (arr[N - 1] < arr[N -2]) {
+            return arr[N - 1];
+        }
         int l = 1;
         int r = N - 2;
-        int mid = 0;
         while (l < r) {
-            mid = l + ((r - l) >> 1);
-            if (arr[mid] > arr[mid - 1]) {
-                r = mid - 1;
-            } else if (arr[mid] > arr[mid + 1]) {
-                l = mid + 1;
+            int m = l + ((r - l) >> 1);
+            if (arr[m] > arr[m - 1]) {
+                r = m - 1;
+            } else if (arr[m] > arr[m + 1]) {
+                l = m + 1;
             } else {
-                return mid;
+                return m;
             }
         }
         return l;
